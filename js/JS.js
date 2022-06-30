807969fd9d0d329b880f10a4bb1eee27e9e60591
Nikita Sinelnikov <nikita.sinelnikov@lenta.ru>
2022-06-30 13:22:32 +0400

    работа над слайдом

--- js/JS.js
+++ js/JS.js
@@ -9,7 +9,21 @@ function init() {
   sliderLine.style.width = width * images.length + "px";
   images.forEach((item) => {
     item.style.width = width + "px";
+    item.style.height = "auto";
   });
+  rollSlider();
 }
-
+window.addEventListener("resize", init);
 init();
+
+document.querySelector(".slider-next").addEventListener("click", function () {
+  count++;
+  if (count >= images.length) {
+    count = 0;
+  }
+  rollSlider();
+});
+
+function rollSlider() {
+  sliderLine.style.transform = "translate(-" + count * width + "px)";
+}
